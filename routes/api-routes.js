4f9076df16529b12e8cbd0cefad7c5e084f8a414
Peter Ting <pting1995@gmail.com>
2021-02-17 21:06:37 -0800

    added post and get routes for api/workouts

---
+++ routes/api-routes.js
@@ -0,0 +1,25 @@
+const router = require("express").Router();
+const workout = require("../models/workout.js");
+
+router.post("/api/workouts/:id", ({ body }, res) => {
+  workout.create(body)
+    .then(dbworkout => {
+      res.json(dbworkout);
+    })
+    .catch(err => {
+      res.status(400).json(err);
+    });
+});
+
+router.get("/api/workouts", (req, res) => {
+  workout.findAll({})
+    .sort({ date: -1 })
+    .then(dbworkout => {
+      res.json(dbworkout);
+    })
+    .catch(err => {
+      res.status(400).json(err);
+    });
+});
+
+module.exports = router;

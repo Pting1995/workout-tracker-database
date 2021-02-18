0d37cfe7ad4c2354e78223aa701a91adefbb8708
Peter Ting <pting1995@gmail.com>
2021-02-17 23:46:59 -0800

    refactored server.js, fixed api routes, fixed models

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,25 +1,29 @@
 const router = require("express").Router();
 const workout = require("../models/workout.js");
 
-router.post("/api/workouts/:id", ({ body }, res) => {
-  workout.create(body)
-    .then(dbworkout => {
-      res.json(dbworkout);
-    })
-    .catch(err => {
-      res.status(400).json(err);
+module.exports = function (app) {
+    router.get("/api/workouts", (req, res) => {
+        workout.find({})
+            // .sort({ date: -1 })
+            .then(dbworkout => {
+                res.json(dbworkout);
+            })
+            .catch(err => {
+                res.json(err);
+            });
     });
-});
 
-router.get("/api/workouts", (req, res) => {
-  workout.find({})
-    .sort({ date: -1 })
-    .then(dbworkout => {
-      res.json(dbworkout);
-    })
-    .catch(err => {
-      res.status(400).json(err);
+    router.post("/api/workouts/:id", ({ body }, res) => {
+        workout.create(body)
+            .then(dbworkout => {
+                res.json(dbworkout);
+            })
+            .catch(err => {
+                res.json(err);
+            });
     });
-});
+}
 
-module.exports = router;
+
+
+// module.exports = router;

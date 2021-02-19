33606f02d9c043459bc28e05ad7b4b5ad0005bf5
Peter Ting <pting1995@gmail.com>
2021-02-18 22:39:30 -0800

    added put route

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,5 +1,5 @@
 const workout = require("../models/workout.js");
-const router = require("express")
+
 
 module.exports = function (app) {
     
@@ -14,13 +14,56 @@ module.exports = function (app) {
             });
     });
 
-    // router.post("/api/workouts", ({ body }, res) => {
-    //     workout.create(body)
-    //         .then(dbworkout => {
-    //             res.json(dbworkout);
-    //         })
-    //         .catch(err => {
-    //             res.json(err);
-    //         });
-    // });
+    app.post("/api/workouts", ({ body }, res) => {
+        workout.create(body)
+            .then(dbworkout => {
+                res.json(dbworkout);
+            })
+            .catch(err => {
+                res.json(err);
+            });
+    });
+
+    app.put("/api/workouts/:id", (req, res) => {
+        workout.findOneAndUpdate({_id: req.params.id},
+        {$push: {exercise: req.body}})
+            .then(dbworkout => {
+                res.json(dbworkout);
+            })
+            .catch(err => {
+                res.json(err);
+            });
+    });
+
+    app.get("/api/workouts/range", (req, res) => {
+
+        workout.find({})
+            .then(dbworkout => {
+                console.log(dbworkout[0])
+                res.json(dbworkout.aggregate([
+                    {
+                        $group:
+                        {
+                            _id: { day: { $dayOfYear: "$date" }},
+                            totalDuration: { $sum: "duration" },
+                            // count: { $sum: 1 }
+                        }
+                    }
+                ]));
+            })
+            .catch(err => {
+                res.json(err);
+            });
+
+        // workout.aggregate([
+        //     {
+        //         $group:
+        //         {
+        //             _id: { day: { $dayOfYear: "$date" }},
+        //             totalDuration: { $sum: "duration" },
+        //             // count: { $sum: 1 }
+        //         }
+        //     }
+        // ])
+    });
 }
